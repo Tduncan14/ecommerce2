53b19a6a477f6b69bfdcbc25ac097c78750b142d
Tre'Shawn Duncan <nbatre1@yahoo.com>
2020-11-12 00:36:56 -0600

    keep moving forward

--- ecommerce/Models/User.js
+++ ecommerce/Models/User.js
@@ -43,3 +43,45 @@ const UserSchema = new mongoose.Schema({
 
 },
 {timeStamp:true})
+
+
+
+// beginning of the virutal field
+
+
+UserSchema.virtual('password')
+.set(function(password){
+    this._password = password
+    this.salt = uuidv1()
+    this.hash_passowrd =this.encryptPassword(password)
+})
+.get(function(){
+
+    return this._password
+})
+
+UserSchema.methods = {
+
+    encryptPassword:function(password) {
+    
+    if(!password) return '';
+
+    try{
+
+        return crypto.createHmac('sha1',this.salt)
+        .update(password)
+        .digest('hex')
+
+    }
+
+
+        catch(err){
+
+            return ''
+        }
+
+    }
+}
+
+
+module.exports = mongoose.model('User',UserSchema);
