e1a258e2a78cdf0bc91835f5269faff2410f2e56
Tre'Shawn Duncan <nbatre1@yahoo.com>
2020-12-07 03:21:27 -0600

    keep moving forward

--- ecommerce/Controllers/Category.js
+++ ecommerce/Controllers/Category.js
@@ -60,3 +60,74 @@ exports.read = (req,res) =>{
 
     return res.json(req.category)
 }
+
+
+exports.update = (req,res,id) =>{
+
+    const category = req.category
+
+    category.name = req.body.name
+
+
+    category.save((err,data) =>{
+
+
+        if(err){
+            return res.status(400).json({
+
+                error:errorHandler(err)
+            });
+        }
+
+        res.json(data)
+
+    })
+
+   
+}
+
+
+
+exports.remove = (req,res) =>{
+
+    const category = req.category
+
+    category.remove((err,data) => {
+ 
+        if(err){
+
+            return res.status(404).json({
+
+                message:"The category has already been deleted or not found"
+            })
+        }
+
+     
+        res.json({
+            message:'Category deleted'
+        })
+
+    })
+
+}
+
+
+
+exports.list = (req,res) =>{
+
+    Category.find().exec((err,data) =>{
+
+        if(err){
+
+            return res.status(400).json({
+
+                error:errorHandler(err)
+            })
+        }
+
+
+       res.json(data)
+
+    })
+
+}
