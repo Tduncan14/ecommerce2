e4b034bdc11ef868792f1dcb91ac9fd6bc70c6e5
tduncan14 <nbatre1@yahoo.com>
2021-01-18 01:37:44 -0800

    keep moving forward

--- emcommerce-front/src/core/Menu.js
+++ emcommerce-front/src/core/Menu.js
@@ -10,9 +10,19 @@ const Menu = () => {
         <ul className="nav nav-tabs bg-primary" >
             <li className="nav-item">
                 <Link className="nav-link" to="/">Home</Link>
+               
+            </li>
+            <li className="nav-item">
+                
                 <Link className="nav-link" to="/signIn">Sign In</Link>
+               
+            </li>
+
+            <li className="nav-item">
+                
                 <Link className="nav-link" to="/signUp"> Sign Up</Link>
             </li>
+
 
         </ul>
     </div>
