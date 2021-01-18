fb86bbc6cd9366a31f636bef8a95b4f0191efaa3
tduncan14 <nbatre1@yahoo.com>
2021-01-17 23:13:32 -0800

    keep moving forwared

---
+++ emcommerce-front/src/core/Menu.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link,withRouter} from 'react-router-dom';
+
+
+const Menu = () => {
+
+
+ return(
+    <div>
+        <ul className="nav nav-tabs bg-primary" >
+            <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/signIn">Sign In</Link>
+                <Link className="nav-link" to="/signUp"> Sign Up</Link>
+            </li>
+
+        </ul>
+    </div>
+ )
+}
+
+export default withRouter(Menu);
---
+++ emcommerce-front/src/user/Signin.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Signin = () => <div>signin</div>
+
+
+
+
+export default Signin
