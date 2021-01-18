ce02ab7672c49d0181987397abf710c8626c2b61
tduncan14 <nbatre1@yahoo.com>
2021-01-18 01:40:57 -0800

    keep moving forward

--- emcommerce-front/src/core/Menu.js
+++ emcommerce-front/src/core/Menu.js
@@ -2,6 +2,20 @@ import React from 'react'
 import {Link,withRouter} from 'react-router-dom';
 
 
+const isActive = (history,path) =>{
+
+
+    if(history.location.pathname === path){
+
+        return {color:'#ff9900'}
+    }
+
+    else{
+        return {color: '#fff'}
+    }
+}
+
+
 const Menu = () => {
 
 
