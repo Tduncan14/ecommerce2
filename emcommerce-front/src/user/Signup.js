d917aadf5c568b66ed02678617d5059adec5d55f
tduncan14 <nbatre1@yahoo.com>
2021-01-20 22:25:43 -0800

    keep moving forward

--- emcommerce-front/src/user/Signup.js
+++ emcommerce-front/src/user/Signup.js
@@ -26,6 +26,22 @@ const Signup = () => {
 
 
      Signup({name,email,password})
+     .then(data =>{
+         if(data.error){
+             setValues({...values, error:data.error, success:false})
+
+         }
+         else{
+             setValues({
+                 ...values,
+                 name:'',
+                 email:'',
+                 password:'',
+                 error:'',
+                 success:true
+             })
+         }
+     })
 
 
     }
@@ -65,17 +81,18 @@ const Signup = () => {
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input onChange={handleChange('name')} type="text" className="form-control"/>
+                <input onChange={handleChange('name')} type="text" className="form-control"
+                value={name}/>
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input onChange={handleChange('email')} type="email" className="form-control"/>
+                <input onChange={handleChange('email')} type="email" className="form-control" value={email}/>
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input onChange={handleChange('password')} type="password" className="form-control"/>
+                <input onChange={handleChange('password')} type="password" className="form-control" value={password}/>
             </div>
 
             <button onClick={clickSubmit} className="btn btn-primary">
@@ -99,12 +116,25 @@ const Signup = () => {
 
      }
 
+     const showError = ()=>(
+         <div className="alert alert-danger" style={{display:error ? '':'none'}}>
+             {error}
+         </div>
+     )
+
+
+     const showSuccess = () => (
+         <div className="alert alert-info" style={{display:success ?'':'none'}}>
+               New Account is created. Please Sign
+         </div>
+     )
 
     return(
     <Layout title="Sign up" description="Sign up to the App"
      className="container col-md-8 offset-md-2">
         
-
+        {showError()}
+        {showSuccess()}
         {signUpForm()}
 
 
