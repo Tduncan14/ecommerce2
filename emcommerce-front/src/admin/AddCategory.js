0a4bb1a1392681f057dae1b0d30180342116f64b
tduncan14 <nbatre1@yahoo.com>
2021-02-08 00:55:51 -0800

    keep moving forward

---
+++ emcommerce-front/src/admin/AddCategory.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import Layout from '../core/Layout';
+import {isAuthenicated} from '../auth';
+import {Link} from 'react-router-dom';
+
+
+const AddCategory = () => {
+
+    const [name,setName] = useState('')
+    const [error,setError] = useState(false)
+    const [success,setSuccess] = useState(false)
+
+
+    const handleChange = (e) => {
+
+        setError('')
+        setName(e.target.value)
+
+
+    }
+
+
+    const clickSubmit = (e) =>{
+
+        e.preventDefault()
+        setError('')
+        setSuccess(false)
+
+        // male request api to create category
+
+
+
+    }
+
+
+
+    // destructure use and token from localStorage
+
+    const{user,token} = isAuthenicated()
+
+
+    const newCategoryForm = () => (
+
+
+      <form onSubmit={clickSubmit}>
+          <div className="form-group">
+              <label className="text-muted">Name                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  </label>
+              <input type="text"
+               className="form-control" 
+               onChange={handleChange}
+               value={name}
+               autoFocus/>
+          </div>
+
+          <button className="btn btn-outline-primary">
+              create Category
+          </button>
+      </form>
+
+
+
+
+
+    )
+
+
+
+}
+
+
+
+export default AddCategory
