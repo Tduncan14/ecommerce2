24be13dda41ec88aff3b4e57c916231dcbaca3d1
tduncan14 <nbatre1@yahoo.com>
2021-02-15 00:08:51 -0800

    keep moving forward

--- emcommerce-front/src/core/Home.js
+++ emcommerce-front/src/core/Home.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Layout from './Layout';
+import {getProducts} from './apiCore';
 
 
-const Home = () => (
+const Home = () => {
 
-    <Layout title="Home Page" description="E-Commerce">
+    const [productsBySell, setproductsBySell] = useState([])
+    const [productByArrival, setProductsByArrival] = useState([])
+    const [error,setError] = useState(false)
 
-    </Layout>
-)
+
+     const loadProductsBySell =() => {getProducts('sold').then(data => {
+
+        if(data.error){
+            setError(data.error)
+        }
+        else{
+            setproductsBySell(data)
+        }
+
+      })
+    }
+
+
+      const loadProductsByArrival = () => {
+
+        getProducts('createdAt').then((data) => {if(data.error){
+            setError(data.error)
+        }
+        else{
+            setProductsByArrival(data)
+        }
+
+    })
+
+      
+    }
+
+
+
+
+   return( <Layout title="Home Page" description="E-Commerce">
+
+    </Layout>)
+}
 
 
 
