028d8d9e1769622b78cc26649cc8c72ed35dbdf1
tduncan14 <nbatre1@yahoo.com>
2021-02-16 14:29:41 -0800

    keep moving forward

--- emcommerce-front/src/core/ShowImage.js
+++ emcommerce-front/src/core/ShowImage.js
@@ -5,13 +5,17 @@ import {API} from '../config';
 
 const ShowImage = ({item,url}) => {
 
-  <div className="product-img">
+
+    console.log(url,'this is the url')
+
+ return( <div className="product-img">
 
 
-        <img  src={`${API}/${url}/${item.id}`}  alt={`${product.name}`}
+        <img  src={`${API}/product/photo/${item._id}`}  alt={`${item.name}`}
         className="mb-3" style={{maxHeight:'100%', maxWidth:"100%"}} />
 
   </div>
+ )
 
 
 
