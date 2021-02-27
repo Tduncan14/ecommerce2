170b5f27b224ff5cd86cb864df42d7ad909c9d1d
tduncan14 <nbatre1@yahoo.com>
2021-02-26 21:58:43 -0800

    keep moving forward

--- emcommerce-front/src/core/Search.js
+++ emcommerce-front/src/core/Search.js
@@ -18,6 +18,9 @@ const Search = () => {
     })
 
 
+    const {categories,category,search,results,searched} = data
+
+
     const loadCategories = () => {
 
         getCategories().then(data => {
@@ -51,11 +54,76 @@ const Search = () => {
 
     },[])
 
+
+
+const searchSubmit = () => {
+
+    //
+
+
+
+}
+
+
+
+const handleChange = () => {
+
+
+
+
+}
+
+
+  const searchForm = () => (
+
+    <form onSubmit={searchSubmit}>
+
+       <span className="input-group-text">
+           <div className="input-group input-group-lg">
+
+              <div className="input-group-prepend">
+                  <select className="btn mr-2" onChange={handleChange('category')}>
+                      <option value="All">Pick Category </option>
+                      {categories.map((c,i) =>(
+
+                          <option key={i} value={c._id}>
+                              {c.name}
+                          </option>
+                      ))}
+                  </select>
+              </div>
+
+
+               <input
+                 type="search"
+                 className="form-control"
+                 onChange={handleChange("search")}
+                 placeholders="Search by name" />
+
+
+           </div>
+
+           <div className="btn input-group-append" style={{border:'none'}}>
+            <button className="input-group-text">Search</button>
+           </div>
+
+
+       </span>
+
+    </form>
+
+
+  )
  
 
     return(
-        <div>
-            
+
+        <div className="row">
+
+            <div className="container">
+                {searchForm()}
+            </div>
+         
         </div>
     )
 
