a70f0df9bebac12ad91bca4c3e533863fd5f708f
tduncan14 <nbatre1@yahoo.com>
2021-03-31 15:41:23 -0700

    keep moving forward

--- emcommerce-front/src/core/Card.js
+++ emcommerce-front/src/core/Card.js
@@ -6,6 +6,29 @@ import ShowImage from './ShowImage';
 const Card = ({product,showViewProductButton= true}) => {
 
 
+    const showViewButton = (showViewProductButton) =>{
+
+        return(
+
+            showViewProductButton && (
+ 
+            <Link to={`/product/${product._id}`} className="mr-2">
+             <button className="btn btn-outline-primary mt-2 mb-2">
+                 View Product
+             </button>
+             </Link>
+            
+
+            )
+
+
+
+
+        )
+
+
+    }
+
 
     return(
         
@@ -13,12 +36,10 @@ const Card = ({product,showViewProductButton= true}) => {
                 <div className="card-header">{product.name}</div>
                 <div className="card-body">
                     <ShowImage item={product} url="product"/>
-                     <p>{product.description.substring(0,100)}</p>
+                     <p className="lead mt-2">{product.description.substring(0,100)}</p>
                      <p>${product.price}</p>
                      <Link to={`/product/${product._id}`}>
-                          <button className="btn btn-outline-primary mt-2 mb-2  mr-2">
-                              View Product
-                          </button>
+                        {showViewButton(showViewProductButton)}
                      </Link>
                      
 
