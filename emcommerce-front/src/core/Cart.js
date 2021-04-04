9a035be9b4e0f50ce07e5440c43e097305f66129
tduncan14 <nbatre1@yahoo.com>
2021-04-04 15:50:50 -0700

    keep moving forward

--- emcommerce-front/src/core/Cart.js
+++ emcommerce-front/src/core/Cart.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import Layout from './Layout';
-import {getProducts} from './apiCore';
+import {getCart} from './cartHelper';
 import Card from './Card';
 
 
@@ -9,6 +9,49 @@ const Cart = () => {
 
 
     const [items, setItems] = useState([]);
+
+
+
+    useEffect(() => {
+
+        setItems(getCart())
+
+
+
+
+
+    },[])
+
+    const showItems = (items) => {
+
+
+
+        return(
+            <div>
+                <h2>Your cart has {`${cart.length}`} items</h2>
+                <hr />
+                {items.map((product,i) => (
+
+                    <Card key={i} product={product} />
+                    
+                ))}
+            </div>
+        )
+
+
+    }
+
+
+    return(
+        <Layout
+         title="Shopping Cart"
+         description="Manage your cart Items. Add remove checkout or continue shopping"
+         className="container-fluid">
+
+
+
+         </Layout>
+    )
 
 
 
